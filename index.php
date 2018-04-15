f159e16ca477db10a4eb515f42203ec77e8cf9f2
TheEdwinMan <32201825+TheEdwinMan@users.noreply.github.com>
2018-04-15 18:12:02 +0200

    bejelentkezett felület elkészítée

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+?>
 <html>
   <head>
     <link rel="stylesheet" href="style.css">
@@ -12,7 +16,7 @@
           $('.aktv').mouseleave(function(){
             $(this).css("background-color", "#303030");
            });
-		   
+
 		 $('#leiras').click(function(){
            $.ajax({
              url: 'leiras.php',
@@ -21,7 +25,72 @@
              }
            });
          });
-		   
+
+		 $('#login').click(function(){
+           $.ajax({
+             url: 'login.php',
+             success: function(data) {
+               $('#in-content').html(data);
+             }
+           });
+         });
+
+       });
+  </script>
+  </head>
+  <body>
+    <div id="space">
+      <div id="logo">
+        <img id="kep" class="nkij" src="logo.png">
+      </div>
+      <div id="Menu">
+        <ul class="cssmenu">
+          <li><div class="aktv" id="leiras"><a class="nkij">Leírás</a></div></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li><div class="aktv" id="login"><a class="nkij">Bejelenzkezés</a></div></li>
+        </ul>
+      </div>
+      <div id="content">
+        <div id="in-content">
+		<?php
+			include 'leiras.php'
+        ?>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
+<?php
+	exit;
+	}
+?>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <title>Epam feladat</title>
+	<script src="http://code.jquery.com/jquery-1.9.0.js"></script>
+    <script>
+    $(document).ready(function(){
+
+         $('.aktv').mouseenter(function(){
+           $(this).css("background-color", "blue");
+          });
+          $('.aktv').mouseleave(function(){
+            $(this).css("background-color", "#303030");
+           });
+
+		 $('#leiras').click(function(){
+           $.ajax({
+             url: 'leiras.php',
+             success: function(data) {
+               $('#in-content').html(data);
+             }
+           });
+         });
+
 		 $('#login').click(function(){
            $.ajax({
              url: 'login.php',

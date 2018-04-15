e52aaea5e90a94a52043c3e47cc59e55984c5320
TheEdwinMan <32201825+TheEdwinMan@users.noreply.github.com>
2018-04-15 17:12:45 +0200

    register form és login form váltakozása

--- login.php
+++ login.php
@@ -9,13 +9,30 @@ if(!isset($_SESSION['username'])){
 			<input type="password" id="password2" placeholder="Jelszó megerősítése">
 			<div class="button" href="#"><a class="nkij">Regisztrálás</a><br></div>
 			<span class = "spanszin" id="error_message"></span
+			<p class = "message">Már regisztáltál? <a onclick = "hide();" href="#">Jelentkezz be</a></p>
 		</form>
-		<form id="login">
+		<form id="loginform">
 			<input type="text" id="username_log" placeholder="Felhasználónév">
 			<input type="password" id="password_log" placeholder="Jelszó">
 			<div class="button" href="#"><a class="nkij">Bejelentkezés</a><br></div>
 			<span class = "spanszin" id="message_log"></span
+			<p class = "message">Nincs fiokod? <a onclick = "hide();" href="#">Regisztrálj</a></p>
 		</form>
+		<script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+		<script>
+			function hide(){
+				var loginform = document.getElementById('loginform');
+				var register = document.getElementById('register');
+				if(loginform.style.display == 'none'){
+					loginform.style.display = 'block';
+					register.style.display = 'none';
+				}
+				else{
+					loginform.style.display = 'none';
+					register.style.display = 'block';
+				}
+			}
+		</script>
 	</div>
 </div>
 <?php

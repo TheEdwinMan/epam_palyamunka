59e96e9b575d748881592f4f6b003cbd36a33176
TheEdwinMan <32201825+TheEdwinMan@users.noreply.github.com>
2018-04-15 18:05:21 +0200

    bejelentkező php fájl a mysql adatbázisba

---
+++ belepes.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+$con = mysqli_connect('localhost','root','','epam');
+mysqli_select_db($con,'users');
+$username = mysqli_real_escape_string($con, $_POST['username']);
+$password = mysqli_real_escape_string($con, $_POST['password']);
+
+$result = mysqli_query($con,"SELECT * FROM users WHERE username='$username' AND password='$password'");
+  if(mysqli_num_rows($result)){
+    $res = mysqli_fetch_array($result);
+    $_SESSION['username'] = $res['username'];
+    $_SESSION['password'] = $res['password'];
+    echo $res['username'];
+  }
+?>

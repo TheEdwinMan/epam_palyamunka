25c6226f68114efef316a511c0189334e51a72f9
TheEdwinMan <32201825+TheEdwinMan@users.noreply.github.com>
2018-04-15 15:38:10 +0200

    kinézet

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <title>Epam feladat</title>
+  </head>
+  <body>
+    <div id="space">
+      <div id="logo">
+        <img id="kep" src="logo.png">
+      </div>
+      <div id="Menu">
+        <ul class="cssmenu">
+          <li><div class="aktv" id="leiras"><a>Leírás</a></div></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li><div class="aktv" id="forms"><a>Bejelenzkezés</a></div></li>
+        </ul>
+      </div>
+      <div id="content">
+        <div id="in-content">
+        </div>
+      </div>
+    </div>
+  </body>
+</html>

6eea29611c7ffb7cce8843b58172ca11680c557f
TheEdwinMan <32201825+TheEdwinMan@users.noreply.github.com>
2018-04-15 18:40:34 +0200

    chat szerkezet

---
+++ chat.php
@@ -0,0 +1,15 @@
+<script src='https://code.jquery.com/jquery-3.3.1.min.js'></script>
+<script>
+</script>
+<div id="kozep">
+  <div class="chat">
+    <div id="uzenetek">
+    </div>
+    <form name="form1" style="color: white;">
+      <div class="send">
+        <textarea name="msg" id="msg" placeholder="Üzenet" onkeypress="return submitChate(event);"></textarea>
+      </div>
+      <div id="chat-gomb" href="#" onclick="submitChat();" ><a>Enter</a></div><br>
+    </form>
+  </div>
+</div>
